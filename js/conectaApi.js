65dffefb0fb6ac9602cf18ef880bcf3d5ab4cb45
Thyago Lobato <thyagollobato@gmail.com>
2023-04-04 15:39:29 -0300

    Criando novos elementos

--- js/conectaApi.js
+++ js/conectaApi.js
@@ -1,11 +1,30 @@
 async function listaVideos() {
-    const conexao = await fetch("http://localhost:3000/videos"); // Lembrar de usar o comando: json-server --watch db.json ou npx json-server --watch db.json par iniciar o servidor.
-    
+    const conexao = await fetch("http://localhost:3000/videos"); // Lembrar de usar o comando: json-server --watch db.json ou npx json-server --watch db.json par iniciar o servidor.    
+    const conexaoConvertida = await conexao.json();
+
+    return conexaoConvertida;
+}
+
+async function criaVideo(titulo, descricao, url, imagem) {
+    const conexao = await fetch("http://localhost:3000/videos", {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json"
+        }, 
+        body: JSON.stringify({
+            titulo: titulo,
+            descricao: `${descricao} mil visualizações`,
+            url: url,
+            imagem: imagem
+        })
+    });
+
     const conexaoConvertida = await conexao.json();
 
     return conexaoConvertida;
 }
 
 export const conectaApi = {
-    listaVideos
+    listaVideos,
+    criaVideo
 }
